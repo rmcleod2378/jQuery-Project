f77163d4d309b3dc90434b1c18f797425f6591c4
rmcleod2378 <44953292+rmcleod2378@users.noreply.github.com>
2019-02-05 21:30:03 -0500

    Add files via upload
    Table Finder Part 2

--- jQuery Project/script.js
+++ jQuery Project/script.js
@@ -1,15 +1,24 @@
 "use strict";
 
 $(document).ready(function () {
+    let tableSelected = null;
+    
+        $(".document").on("click", ".available", function (e) {
+        $("form").fadein(2000);
+            tableSelected = e.target
+    })
 
+        $(".document").on("click", ".savebtn", function (e) {
+        $(tableSelected).removeClass(".available").addClass(".reserved");
+        $(form).hide();
+    })
+    
+        $(document).on("mouseenter", ".available", function (e) {
+        $(e.target).toggleClass("hover");
+    })
 
-
-
-
-
-
-
-
-
+        $(document).on("mouseleave", ".available", function (e) {
+        $(e.target).toggleClass("hover");
+    })
 
 });
